8c315063dcee075d8bc2cde90aef9d6eaa9fdbe5
Minortal01 <107630482+Minortal01@users.noreply.github.com>
2022-06-21 01:49:35 +0300

    Delete task_4.py

--- Minortal/Python/Python_training_course/Section_2_My_first_steps_in_python/Subsection_2.3_Mathematical_functions_and_the_math_module/task_4.py
+++
@@ -1,7 +0,0 @@
-#Допишите текст программы для вычисления евклидового расстояния (гипотенузы) по перемещениям a и b.
-#Sample Input:3 4
-#Sample Output:5.0
-a, b = map(int, input(). split())
-import math
-x = (math.sqrt(a**2 + b**2 ))
-print(round(x, 2))
