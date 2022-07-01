a820994a7b9dce1a5e3ea24e83a4e46214849161
Minortal01 <107630482+Minortal01@users.noreply.github.com>
2022-07-01 17:29:36 +0300

    Delete Minortal/Python/Python_training_course/Section_2_My_first_steps_in_python directory

--- Minortal/Python/Python_training_course/Section_2_My_first_steps_in_python/Subsection_2.4_Print and input functions/task_3.py
+++
@@ -1,7 +0,0 @@
-#Sample Input:
-#Sample Output: Hello Balakirev
-# put your python code here
-s1 = "Hello"
-s2 = "Balakirev"
-print(s1, end=' ' )
-print(s2)
--- Minortal/Python/Python_training_course/Section_2_My_first_steps_in_python/Subsection_2.4_Print and input functions/task_4.py
+++
@@ -1,7 +0,0 @@
-#Sample Input: I love
-#Sample Output: Word 1: I | Word 2: love
-# ввод данных
-s1, s2 = map(str.strip, input().split())
-# здесь продолжите программу
-print("Word 1:",s1, end=' | ')
-print("Word 2:",s2,)
