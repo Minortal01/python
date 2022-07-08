2d62cd3f88a6562d3bc8c35625618ae0c3728036
Minortal01 <107630482+Minortal01@users.noreply.github.com>
2022-07-08 12:34:26 +0300

    Add files via upload

---
+++ Python_-_обучающий_курс/2.Мои_первые_шаги_в_Python/2.4_Функции_print_и_input/task_3.py
@@ -0,0 +1,11 @@
+#В программе определите две следующие строки:
+#s1 = "Hello"
+#s2 = "Balakirev"
+#С помощью двух функций print (каждая отображает соответствующую строку) отобразите строки в консоли так, чтобы они шли друг за другом через пробел.
+#Sample Input:
+#Sample Output: Hello Balakirev
+# put your python code here
+s1 = "Hello"
+s2 = "Balakirev"
+print(s1, end=' ' )
+print(s2)
