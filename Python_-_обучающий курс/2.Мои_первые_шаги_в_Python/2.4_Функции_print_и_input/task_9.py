0a63be84c9f5bb71b1074da976f19a579304cf87
Minortal01 <107630482+Minortal01@users.noreply.github.com>
2022-07-08 12:29:17 +0300

    Delete Python_-_обучающий курс directory

--- Python_-_обучающий курс/2.Мои_первые_шаги_в_Python/2.4_Функции_print_и_input/task_9.py
+++
@@ -1,6 +0,0 @@
-#Напишите программу вывода переменной math.pi с точностью до тысячных (три знака после запятой).
-#Sample Input:
-#Sample Output: 3.142
-# put your python code here
-import math
-print(round(math.pi, 3))
