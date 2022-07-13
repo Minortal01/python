3d3a204dc76b9268a4f04fbcd49d3b145288e319
Minortal01 <107630482+Minortal01@users.noreply.github.com>
2022-07-13 21:17:32 +0300

    Update task_9.py

--- Python_-_обучающий_курс/2.Мои_первые_шаги_в_Python/2.4_Функции_print_и_input/task_9.py
+++ Python_-_обучающий_курс/2.Мои_первые_шаги_в_Python/2.4_Функции_print_и_input/task_9.py
@@ -1,6 +1,7 @@
-#Напишите программу вывода переменной math.pi с точностью до тысячных (три знака после запятой).
-#Sample Input:
-#Sample Output: 3.142
+# Напишите программу вывода переменной math.pi с точностью до тысячных
+# (три знака после запятой).
+# Sample Input:
+# Sample Output: 3.142
 # put your python code here
 import math
 print(round(math.pi, 3))
