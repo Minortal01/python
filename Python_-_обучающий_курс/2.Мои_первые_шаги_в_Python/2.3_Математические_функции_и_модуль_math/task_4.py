1cd82c2eced818a10a6db8dca921dde11b9f1555
Minortal01 <107630482+Minortal01@users.noreply.github.com>
2022-07-13 21:03:25 +0300

    Update task_4.py

--- Python_-_обучающий_курс/2.Мои_первые_шаги_в_Python/2.3_Математические_функции_и_модуль_math/task_4.py
+++ Python_-_обучающий_курс/2.Мои_первые_шаги_в_Python/2.3_Математические_функции_и_модуль_math/task_4.py
@@ -1,7 +1,8 @@
-#Допишите текст программы для вычисления евклидового расстояния (гипотенузы) по перемещениям a и b.
-#Sample Input:3 4
-#Sample Output:5.0
+# Допишите текст программы для вычисления евклидового расстояния (гипотенузы)
+# по перемещениям a и b.
+# Sample Input:3 4
+# Sample Output:5.0
 import math
 a, b = map(int, input(). split())
-x = (math.sqrt(a**2 + b**2 ))
+x = (math.sqrt(a**2 + b**2))
 print(round(x, 2))
