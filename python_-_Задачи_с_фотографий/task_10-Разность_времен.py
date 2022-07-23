ddaba4f946509dbf050c746c31584d4ab8b49727
Minortal01 <107630482+Minortal01@users.noreply.github.com>
2022-07-23 04:06:15 +0300

    Add files via upload

---
+++ python_-_Задачи_с_фотографий/task_10-Разность_времен.py
@@ -0,0 +1,16 @@
+# Входные данные:
+# 1
+# 1
+# 1
+# 2
+# 2
+# 2
+# Вывод данных: 3661
+
+a = int(input())
+b = int(input())
+c = int(input())
+x = int(input())
+y = int(input())
+z = int(input())
+print((x-a)*3600 + (y-b)*60 + z - c)
---
+++ python_-_Задачи_с_фотографий/task_16-Следущее_и_предыдущее.py
@@ -0,0 +1,9 @@
+# Входные данные:179
+
+# Вывод программы:
+# The next number for the number 179 is 180
+# The previous number for the number 179 is 178
+
+a = int(input())
+print('The next number for the number ' + str(a) + ' is ' + str(a + 1) + '.')
+print('The previous number for the number ' + str(a) + ' is ' + str(a - 1) + '.')
