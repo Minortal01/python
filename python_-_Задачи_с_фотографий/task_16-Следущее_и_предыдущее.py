e3476300c54a91308b94fcbae21da33728d0185a
Minortal01 <107630482+Minortal01@users.noreply.github.com>
2022-07-25 13:19:55 +0300

    Update task_16-Следущее_и_предыдущее.py

--- python_-_Задачи_с_фотографий/task_16-Следущее_и_предыдущее.py
+++ python_-_Задачи_с_фотографий/task_16-Следущее_и_предыдущее.py
@@ -5,5 +5,5 @@
 # The previous number for the number 179 is 178
 
 a = int(input())
-print('The next number for the number ' + str(a) + ' is ' + str(a + 1) + '.')
-print('The previous number for the number ' + str(a) + ' is ' + str(a - 1) + '.')
+print('The next number for the number '+str(a)+' is ' + str(a + 1) + '.')
+print('The previous number for the number ' + str(a)+' is '+str(a - 1) + '.')
