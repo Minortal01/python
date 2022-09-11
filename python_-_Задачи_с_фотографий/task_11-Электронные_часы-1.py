b1a9fda168074eeb2d7f6c70ac88b828afc058cd
Minortal01 <107630482+Minortal01@users.noreply.github.com>
2022-09-12 01:09:18 +0300

    Update task_11-Электронные_часы-1.py

--- python_-_Задачи_с_фотографий/task_11-Электронные_часы-1.py
+++ python_-_Задачи_с_фотографий/task_11-Электронные_часы-1.py
@@ -2,12 +2,7 @@
 
 # Вывод программы:2 30
 
-n = int(input())
-if n < 1440:
-    print (n // 60)
-    print (n % 60)
-else:
-    a = 1440 - n
-    a *= -1
-    print(a // 60)
-    print (a % 60)
+a = int(input())
+b = a // 60 % 24
+c = a % 60
+print(b, c)
